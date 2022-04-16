e4f32c5dcd7869967ffdb4409ac588d7e9580a64
Santiago Ponce <saponce@ucsc.edu>
2022-04-16 12:36:26 -0700

    Changed controls text

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -33,12 +33,13 @@ class Menu extends Phaser.Scene {
             }
 
             // show menu text
-            this.add.text(game.config.width/2, game.config.height/2 - borderUISize
-             - borderPadding, 'ROCKET PATROL', menuConfig).setOrigin(0.5);
-             this.add.text(game.config.width/2, game.config.height/2,
-              'Use <--> arrows to move & (F) to fire', menuConfig).setOrigin(0.5);
+            this.add.text(game.config.width/2 - 640/2 + 20, game.config.height/2 + 0, "Controls:", menuConfig);
+            this.add.text(game.config.width/2 - 640/2 + 20, game.config.height/2 + 30, "Use <--> arrows to move", menuConfig);
+            this.add.text(game.config.width/2 - 640/2 + 20, game.config.height/2 + 60, "Press (F) to fire", menuConfig);
+
             menuConfig.backgroundColor = '#80FF00';
             menuConfig.color = '#000';
+            
             this.add.text(game.config.width/2, game.config.height/2 + borderUISize + 
             borderPadding, 'Press <- for Novice or -> for Expert', menuConfig).setOrigin(0.5);
       
